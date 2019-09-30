aff1141a656c75fea0fd73db141453e23346a40e
daichaso0307 <info@toyamadaichi.com>
2019-09-30 07:57:28 +0000

    rails03

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'pages#index'
   get 'pages/help'
+  
+  #resources :users
+  
+  get 'users' => 'users#index', as: :users
+  post 'users' => 'users#create', as: :create_user
+  get 'users/new' => 'users#new', as: :new_user
+  # get 'users/:id/edit' => 'users#edit', as: :edit_user
+  # get 'users/:id/edit' => 'users#show', as: :user
+  # patch 'users/:id/edit' => 'users#update', as: :show_user
+  # put 'users/:id/edit' => 'users#update', as: :update_user
+  # delete 'users/:id/edit' => 'users#destroy', as: :destroy_user
 end
