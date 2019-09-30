37f4b9e8de8b41812ebcea80a85e4e2ab5345baf
daichaso0307 <info@toyamadaichi.com>
2019-09-30 08:14:15 +0000

    rails04 kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  VALID_EMAIL_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
+  
+  validates :name, presence: true, uniqueness: true, length: {maximum: 15}
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  validates :password, format: {with: VALID_PASSWORD_REGEX}
   
   has_secure_password
 end
